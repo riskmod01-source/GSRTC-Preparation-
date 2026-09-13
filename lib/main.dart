77a710aeaa0e6369f9d443e366832c74a621476e
riskmod01-source <riskmod01@gmail.com>
2026-09-13 12:09:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class MasterAppRouter extends StatefulWidget {
 }
 
 class _MasterAppRouterState extends State<MasterAppRouter> {
-  static const String _adminEmail = 'thakor.xyz.admin@gsrtc.in';
+  static const String _adminEmail = 'riskmod01@gmail.com'
   int _currentStep = 1;
   String _currentUserEmail = '';
   String _currentUserName = '';
